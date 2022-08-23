d56f24bb11e58b804a7436516f1eae4acb2e6a43
Tanner Richards <Tanner.richards990@gmail.com>
2022-08-23 11:22:12 -0700

    for some reasons my prior commits didnt register, had some vs code load error i had to restore, but finishing up the todos.js now

--- todos/todos.js
+++ todos/todos.js
@@ -15,28 +15,63 @@ const todoForm = document.querySelector('.todo-form');
 const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
-todoForm.addEventListener('submit', async(e) => {
+todoForm.addEventListener('submit', async (e) => {
     // on submit, create a todo, reset the form, and display the todos
+    e.preventDefault();
+    const formData = new FormData(todoForm);
+    const todo = formData.get('todo');
+    await createTodo(todo);
+    e.target.reset();
+    displayTodos();
 });
 
-// create todo state
+// create todo state 
+let todoArr = [];
 
+// 
 // add async complete todo handler function
-    // call completeTodo
-    // swap out todo in array
-    // call displayTodos
+// async function completeTodo(id)
+//  // call completeTodo function
+// completeTodo();
+//  // swap out todo in array with completed todo object (with completed: true)
+// if (todos.complete) {
+//     todos[i] = {
+//         todo: todos, 
+//         completed: true,
+//     };
+// }
+// update the todo in the array
+// render the todo list
 
-   
+
+// call displayTodos
+    
 
 async function displayTodos() {
     // clear the container (.innerHTML = '')
-    // display the list of todos, 
-          // call render function, pass in state and complete handler function!
-          // append to .todos
+    const todos = await getTodos(); 
+    todosEl.textContent = '';
+    
+    // call render function, pass in state and complete handler function!
+    for (let todo of todos) {
+        const todoEl = renderTodo(todo);
+
+        todoEl.addEventListener('click', async () => {
+            await completeTodo(todo.id);
+        });
+        todosEl.append(todoEl);
+    }
+
+    
+        
+        
 }
 
 // add page load function
-    // fetch the todos and store in state
+window.addEventListener('load', displayTodos);
+    // fetch the todos and store in state   
+
+
     // call displayTodos
 
 logoutButton.addEventListener('click', () => {
@@ -44,8 +79,12 @@ logoutButton.addEventListener('click', () => {
 });
 
 
-deleteButton.addEventListener('click', async() => {
+deleteButton.addEventListener('click', async () => {
     // delete all todos
-    // modify state to match
+    await deleteAllTodos();
+    // modify state to match new state (empty array)
+    todoArr = []; 
+    
+    displayTodos();
     // re displayTodos
 });
